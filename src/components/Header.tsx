9f49c1fafdb4732caa0d6706bc45f2facba1f1e0
WIll Stansill <willstansill@gmail.com>
2024-08-01 23:53:53 -0400

    Next Commimt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,11 +5,11 @@ import { PrismicNextLink } from "@prismicio/next";
 import NavBar from "@/components/NavBar";
 
 export default async function Header() {
-    const client = createClient();
-    const settings = await client.getSingle("settings");
-    return (
-        <header className="top-0 z-50 mx-auto max-w-7x1 md: sticky md:top-4">
-            <NavBar settings={settings}/>
-        </header>
-    )
+  const client = createClient();
+  const settings = await client.getSingle("settings");
+  return (
+    <header className="top-0 z-50 mx-auto max-w-7x1 md: sticky md:top-4">
+      <NavBar settings={settings} />
+    </header>
+  );
 }
